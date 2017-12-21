4baaf6cf9beb6dc6007371a57620be1822216ad6
CarlEarlZhou <carl710082796@hotmail.com>
2017-12-21 23:05:10 +0800

    basic question page ui

--- tabby/urls.py
+++ tabby/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^new_question/$', views.newQuestion, name='new_question')
+    url(r'^new_question/$', views.newQuestion, name='new_question'),
+    url(r'^question/$', views.question, name='question')
 ]
